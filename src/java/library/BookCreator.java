acfda5a2301c6055d84bb75f2d368e6449ed768a
pupil <pupil@ivkhk.local>
2018-12-11 11:15:08 +0200

    Возврат книги читателю

---
+++ src/java/library/BookCreator.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package library;
+
+import entity.Book;
+import java.util.Scanner;
+
+/**
+ *
+ * @author pupil
+ */
+public class BookCreator {
+    public Book returnNewBook(){
+        Scanner scnner = new Scanner (System.in);
+        System.out.println("------------Добавление новой книги-----------");
+        Book book = new Book();
+        System.out.println("Название книги:");
+        book.setBookName(scanner.nextLine());
+        System.out.println("Автор книги:");
+        book.setBookName(scanner.nextLine());
+        System.out.println("Год издания книги:");
+        book.setYearPublishing(scanner.nextInt());
+        return book;
+    }
+}
